fad1b35565938dd6d6ab51d017f065522067b9b5
WALTER <arish.gzp988@gmail.com>
2023-02-27 22:42:52 +0530

    suggestion component styled

--- src/components/Suggestion.jsx
+++ src/components/Suggestion.jsx
@@ -4,26 +4,53 @@ import '../components/Suggestion.css';
 const Suggestion = () => {
 
     const [user, setUser] = useState(null);
+    
+
+    
 
     useEffect(() => {
-        fetch("https://randomuser.me/api/")
-          .then((response) => response.json())
-          .then((data) => setUser(data.results[0]));
-      }, []);
-    
-      if (!user) {
-        return <div>Loading...</div>;
-      }
-
+      fetch("https://randomuser.me/api/")
+        .then(response => response.json())
+        .then(data => setUser(data.results[0]));
+    }, []);
+  
+    if (!user) {
+      return <div>Loading...</div>;
+    }
+  
 
   return (
     <>
     <div className="sugg">
-     <ul>
+     <div className="dp-cont"> <img className="dp" src={user.picture.large}></img>
+     <div className="username-head-text"> {user.name.first} {user.name.last} <div className="Uid-text">{user.login.username} <div className="swicth-btn"> <a href="#">Switch</a></div></div></div>
+     
 
-        <li>  <img className="dp-image" src={user.picture.medium}></img>   <h3>{user.name.first} {user.name.last}</h3>  </li>
-        <li> {user.login.username} </li>
-     </ul>
+     </div>
+     <div className="suggestions-list">
+      <h6>Suggestions for you</h6>
+
+      <div className="dp-cont"> <img className="dp" src={user.picture.large}></img>
+     <div className="username-head-text"> {user.name.first} {user.name.last} <div className="Uid-text">{user.login.username} <div className="follow-btn"> <a href="#">Follow</a></div></div></div></div>
+
+     <div className="dp-cont"> <img className="dp" src={user.picture.large}></img>
+     <div className="username-head-text"> {user.name.first} {user.name.last} <div className="Uid-text">{user.login.username} <div className="follow-btn"> <a href="#">Follow</a></div></div></div></div>
+
+     <div className="dp-cont"> <img className="dp" src={user.picture.large}></img>
+     <div className="username-head-text"> {user.name.first} {user.name.last} <div className="Uid-text">{user.login.username} <div className="follow-btn"> <a href="#">Follow</a></div></div></div></div>
+     
+
+      
+     </div>
+     
+     
+     
+   
+
+
+
+
+
 
       
     </div>
